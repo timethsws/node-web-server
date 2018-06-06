b1f0a34ce3c4ff71e90a1ef742072aefb812b181
Timeth Subasinghe <timethsws@gmail.com>
2018-06-06 11:38:24 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs')
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`
+        console.log('Log : ',log);
+
+    fs.appendFile('server.log', log + '\n', (err)=> {
+        console.log('Unable to append to server.log')
+    })
+    next();
+})
+
+// app.use((req, res, next)=> {
+//     res.render('maintenance.hbs',{
+//         pageTitle : 'Maintenance Mode'
+//     })
+// });
+
+app.use(express.static(__dirname + '/public'))
+
+hbs.registerHelper('getCurrentYear',() =>{
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt',(text) =>{
+    return text.toUpperCase()
+})
+
+app.get('/' , (req,res) => {
+    res.render('home.hbs',{
+        pageTitle : 'Home Page',
+        welcomeMessage : ' Welcome !'
+    })
+})
+
+app.get('/about', (req,res) =>{
+    res.render('about.hbs',{
+        pageTitle : 'About Page',
+    })
+})
+
+app.get('/bad', (req,res) =>{
+    res.send({
+        'ErrorMessage' : "Booom"
+    })
+})
+
+app.listen(3000, () => {
+    console.log('server is up on port 3000')
+});
